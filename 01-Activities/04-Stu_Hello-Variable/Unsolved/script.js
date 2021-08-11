ca66cb389bffb2e28799a89efea0020f5aae44f2
Kartiki Saraf <kartikisaraf@yahoo.com>
2021-08-11 16:34:12 -0700

    Changed index.html and script.js in Activities 02-Stu_Script_ConsoleLog folder.

--- 01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,29 @@
 // Write Your JavaScript Code Here
+
+// Declares student variable using var keyword 
+var personName;
+var pets;
+var funFact;
+
+// Logs "My name and number of pets"
+console.log("My name is " + personName + ".");
+console.log("I have " + pets + " pets.");
+
+// Uses assignment operator(=) to assign a value
+var personName = "Katie";
+var pets = 3;
+
+// Logs "My name and number of pets"
+console.log("My name is " + personName + ".");
+console.log("I have " + pets + " pets.");
+
+// To re-assign a variable, use only the variable's name  
+personName = "Kartiki";
+pets = 2;
+
+// Logs "My name and number of pets"
+console.log("My name is " + personName + ".");
+console.log("I have " + pets + " pets.");
+
+funFact = "More human twins are being born now than ever before.";
+console.log(funFact)
